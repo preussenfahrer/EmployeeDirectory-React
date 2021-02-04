a8a561d3102e29d665e075ebe11f58b00b10678d
Pere Grinus <gregjleighton@gmail.com>
2021-02-04 16:49:21 -0500

    removed EmployeeContainer.js.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Table from "./components/Table";
 import Header from "./components/Header";
 import UserCard from "./components/UserCard";
-// import TableRow from "./components/TableRow";
+import TableRow from "./components/TableRow";
 // import Input from "./components/Input";
 // Input EmployeeContainer from "./components/EmployeeContainer"
 
@@ -23,7 +23,7 @@ function App() {
 
       <UserCard />
 
-      {/* <TableRow /> */}
+      <TableRow />
 
       {/* <Input /> */}
 
