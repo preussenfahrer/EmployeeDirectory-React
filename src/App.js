b7c2fea4c03cc3fbe39abdf51925e8426ae6abc0
Pere Grinus <gregjleighton@gmail.com>
2021-02-04 16:17:25 -0500

    removed bp code from Header.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Table from "./components/Table";
 
+
 function App() {
   return (
     <div className="App">
@@ -11,5 +12,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
