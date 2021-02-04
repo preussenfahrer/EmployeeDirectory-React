3b0f28bc63d35b80eb43cf93a1287f1bd7f9e883
Pere Grinus <gregjleighton@gmail.com>
2021-02-04 16:34:07 -0500

    UserCard content is rendering in App.js. Need to style. Now moving on to render remaining component files and begin styling page.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Table from "./components/Table";
 import Header from "./components/Header";
-// import UserCard from "./components/UserCard";
+import UserCard from "./components/UserCard";
 // import TableRow from "./components/TableRow";
 // import Input from "./components/Input";
 // Input EmployeeContainer from "./components/EmployeeContainer"
@@ -21,7 +21,7 @@ function App() {
 
       <Table />
 
-      {/* <UserCard /> */}
+      <UserCard />
 
       {/* <TableRow /> */}
 
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function UserCard() {
-    
+    return (
+        <div className="card">
+            <h2>User Card</h2>
+        </div>
+    )
 }
 
 export default UserCard;
