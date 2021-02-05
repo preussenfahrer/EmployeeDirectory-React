1a3f97e3d0e2436b2cd1330a9b3529a3aed2df5e
Pere Grinus <gregjleighton@gmail.com>
2021-02-05 10:08:44 -0500

    added API.js to new utils folder

---
+++ src/utils/API.js
@@ -0,0 +1,8 @@
+import React from "react";
+// axios.get('https://randomuser.me/api/?results=25')
+//             .then((res) => {
+//                 this.setState((state, props) => {
+//                     return{users: res.data.results}
+//                 })
+//                 console.log(this.state);
+//             })
